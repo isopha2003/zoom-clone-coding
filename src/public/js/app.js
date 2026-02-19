754026e87b3ff772af53ecec9c744effcc2c063f
isopha2003@gmail.com <isopha2003@gmail.com>
2026-02-19 18:47:48 +0900

    Add Websocket Message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hello");
+const socket = new WebSocket(`ws://${window.location.host}`);
+// 브라우저에서 방금 만든 백엔드 서버로 실시간 연결을 요청하는 첫 번째 단계
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from the Server");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
-import http from "http";
-import express from "express";
-import WebSocket from "ws";
+import http from "http"; // Node.js 기본 HTTP 서버 모듈
+import express from "express"; // 웹 프레임워크
+import WebSocket from "ws"; // 웹소켓 라이브러리
 
-const app = express();
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
+const app = express(); 
+app.set("view engine", "pug"); // 뷰 엔진으로 pug 사용
+app.set("views", __dirname + "/views"); // 템플릿 파일 위치 설정
+app.use("/public", express.static(__dirname + "/public")); // 정적 파일(JS, CSS) 경로 설정
+// 사용자에게 보여줄 화면(UI)과 정적 파일들을 어떻게 처리할지 정의
+
+app.get("/", (_, res) => res.render("home")); 
+// 사용자가 http://localhost:3000/에 접속하면 home.pug 파일을 그러셔(render) 보여줌
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
+// 서버 통합
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
+// server 라는 변수에 HTTP와 WebSocket이 모두 담겨 있음
+// 이렇게 하면 3000번 포트 하나로 웹 사이트 접속(HTTP)과 실시간 통신(WS)을 동시에 할 수 있음
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser");
+    });
+    socket.on("message", (message) => {
+        console.log(message.toString("utf-8"));
+    })
+    socket.send("hello");
+});
+// 새로운 클라이언트가 웹소켓 연결을 시도해서 성공하면 함수를 실행
 
 server.listen(3000, handleListen);
